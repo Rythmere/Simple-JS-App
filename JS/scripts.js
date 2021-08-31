704e74b5c7f4b191fd7effe94d5f96215c4b3fb0
Rythmere <rythmere@gmail.com>
2021-08-31 17:07:04 -0500

    added pokemonList array and removed extra < from index.html
    Added 3 pokemon entries to the pokemonList array and gave name, height, and type to each in an object.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favouriteFood = 'Pizza';
-document.write(favouriteFood);
+let pokemonList = [
+  {name: 'Pikachu', height: 0.4, type: ['Electric']},
+  {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
+  {name: 'Mew', height: 0.4, type: ['Psychic']}
+];
