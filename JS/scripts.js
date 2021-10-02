bfa2629d9f54c81b47072e93031814130afa228e
Rythmere <rythmere@gmail.com>
2021-10-02 18:52:33 -0500

    Added for loop and if else for Pokemonlist array.
    For loop prints out the names and heights of all pokemon within the array
    If else finds the largest pokemon and add a extra statement to its height.
    (If else currently checking if height is 1.5 or higher)

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,11 @@ let pokemonList = [
   {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
   {name: 'Mew', height: 0.4, type: ['Psychic']}
 ];
+for (var i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 1.5) {
+  document.write((pokemonList[i].name) + " (height:" + (pokemonList[i].height) +") - Wow, that's big!"+"</br></br>");
+} else {
+    document.write((pokemonList[i].name) + " (height:" + (pokemonList[i].height) +")"+"</br></br>");
+}
+
+}
