2ce5f2a51b9f4b80d307ecf92213be44926b8f42
Rythmere <rythmere@gmail.com>
2021-10-24 06:56:55 -0500

    Added A IIFE and forEach loop
    Created a IIFE for the pokemon Array with functions to add to the array and return the array for use outside of the IIFE. Replace the for loop with a forEach.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,27 @@
-let pokemonList = [
-  {name: 'Pikachu', height: 0.4, type: ['Electric']},
-  {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-  {name: 'Mew', height: 0.4, type: ['Psychic']}
-];
-for (var i = 0; i < pokemonList.length; i++) {
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Pikachu', height: 0.4, type: ['Electric']},
+    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
+    {name: 'Mew', height: 0.4, type: ['Psychic']}
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+pokemonList = pokemonRepository.getAll();
+pokemonList.forEach((item, i) => {
   if (pokemonList[i].height >= 1.5) {
   document.write((pokemonList[i].name) + " (height:" + (pokemonList[i].height) +") - Wow, that's big!"+"</br></br>");
 } else {
     document.write((pokemonList[i].name) + " (height:" + (pokemonList[i].height) +")"+"</br></br>");
 }
-
-}
+});
