331f7546f8a913444a0ad16e878dc50b2be8bbbd
Rythmere <rythmere@gmail.com>
2021-10-27 09:17:49 -0500

    added loadList, loadDetails, ShowLoadingMessage, and hideLoadingMessage functions
    loadList function gets the pokemon info from the pokeapi and fills out the pokemonList array.
    loadDetails function takes the object from the array for the button clicked and logs the pokemons name, picture , height, and types to the console.
    ShowloadingMessage function add a loading message to the page while loadList and loadDetails in retrieving the data from pokeapi.
    hideLoadingMessage function removed the loading message once the data has been retrieved.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,6 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Pikachu', height: 0.4, type: ['Electric']},
-    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-    {name: 'Mew', height: 0.4, type: ['Psychic']}
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
@@ -16,7 +13,7 @@ let pokemonRepository = (function () {
     let pokedex = document.querySelector('.pokemon-list');
     let pokedexEntry = document.createElement('li');
     let pokedexButton = document.createElement('button');
-    pokedexButton.innerText = pokemon;
+    pokedexButton.innerText = pokemon.name;
     pokedexButton.classList.add("pokemonButton");
     pokedexEntry.appendChild(pokedexButton);
     pokedex.appendChild(pokedexEntry);
@@ -30,16 +27,75 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
+
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      })
+    }).then(function () {
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+  }
+
+  function loadDetails(item) {
+    showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).then(function () {
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+  }
+
+  function showLoadingMessage() {
+    let loading =document.querySelector('.pokemon-list');
+    let loadingMessage = document.createElement('h2');
+    loadingMessage.innerText = 'Loading';
+    loading.appendChild(loadingMessage);
+  }
+
+  function hideLoadingMessage() {
+    let loading =document.querySelector('.pokemon-list');
+    let remove = document.querySelector('h2');
+    loading.removeChild(remove);
+  }
+
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    loadList: loadList,
+    addListItem: addListItem,
+    loadDetails: loadDetails,
+    showLoadingMessage: showLoadingMessage,
+    hideLoadingMessage: hideLoadingMessage
   };
 })();
-pokemonList = pokemonRepository.getAll();
-pokemonList.forEach((item, i) => {
-  pokemonRepository.addListItem(pokemonList[i].name);
+
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
