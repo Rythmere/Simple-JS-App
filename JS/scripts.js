1b231aa64fdcd83e27c245a4f453c15454b60bf4
Rythmere <rythmere@gmail.com>
2021-10-30 10:43:01 -0500

    Created show and hide modals.
    added show and hide modal functions to show details so the modal pops up when a button in the pokemon list is clicked and addd the name, height and img of pokemon to modal.

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,6 +9,8 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+/* adds first 151 pokemon from api to the ul and creates buttons with each ones name in the ul */
+
   function addListItem(pokemon) {
     let pokedex = document.querySelector('.pokemon-list');
     let pokedexEntry = document.createElement('li');
@@ -20,17 +22,23 @@ let pokemonRepository = (function () {
     buttonEvent(pokedexButton, pokemon);
   }
 
+/* opens modal when button is clicked by running showDetails function*/
+
   function buttonEvent(pokedexButton, pokemon) {
     pokedexButton.addEventListener('click', function (event) {
       showDetails(pokemon);
     });
   }
 
+  /* runs loadDetails function to pull the pokemons details from the api, then runs showModal function*/
+
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      showModal(pokemon);
     });
   }
+
+  /* retrieves pokemon list from the api */
 
   function loadList() {
     showLoadingMessage();
@@ -51,6 +59,8 @@ let pokemonRepository = (function () {
       hideLoadingMessage();
     });
   }
+
+  /* retrieves pokemon details from api*/
 
   function loadDetails(item) {
     showLoadingMessage();
@@ -81,6 +91,62 @@ let pokemonRepository = (function () {
     let remove = document.querySelector('h2');
     loading.removeChild(remove);
   }
+
+/* Creates modal and sets it visible with is-visible class, sets event listeners to remove modal with esc, clicking outside of the model, and the close button*/
+
+  function showModal(pokemon) {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.innerHTML = "";
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key ==='Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = pokemon.name;
+
+  let pokemonImg = document.createElement('img');
+  pokemonImg.src = pokemon.imageUrl;
+
+  let pokemonHeight = document.createElement('p');
+  pokemonHeight.innerText = "Height: " + pokemon.height;
+
+  /*let pokemonType = document.createElement('p');
+  pokemonType.innerText = 'Types: ' + pokemon.types; */
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(pokemonImg);
+  modal.appendChild(pokemonHeight);
+  /*modal.appendChild(pokemonType);*/
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+/* removes is-visible class from modal container */
+
+function hideModal() {
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.remove('is-visible');
+}
 
 
   return {
